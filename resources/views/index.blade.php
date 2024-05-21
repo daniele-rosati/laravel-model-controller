bb28458264e8ceba0bfe8ea3ada9eb73a6ff025d
daniele-rosati <drosati151@gmail.com>
2024-05-21 18:36:11 +0200

    can't make it works

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,16 +8,17 @@
     <div class="container">
          <h1 class="text-center mt-3">Movies</h1>
          <div class="row row-cols-1 row-cols-md-2 g-4 my-3"></div>
-        
+         
+         
                     <div class="col">
 
                         <div class="card shadow h-100">
                             <div class="card-body">
-                                <h4 class="card-title"></h4>
-                                <p><strong>Original title: </strong></p>
-                                <p><strong>Nationality: </strong></p>
-                                <p><strong>Released on: </strong></p>
-                                <p><strong>Vote on: </strong></p>
+                                <h4 class="card-title">title </h4>
+                                <p><strong>Original title: </strong>{{ $movie->original_title }}</p>
+                                <p><strong>Nationality: </strong>{{ $movie->nationality }}</p>
+                                <p><strong>Released on: </strong>{{ $movie->date }}</p>
+                                <p><strong>Vote on: </strong>{{ $movie->vote }}</p>
                             </div>
                         </div>
 
@@ -28,7 +29,7 @@
                     <div class="col">
                         <h2>No Movies in the database! 😥</h2>
                     </div>
-                    
+                
 
             </div>
     </div>
